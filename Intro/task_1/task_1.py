13e7186cfe3defeb7aad1a834b71135c52ada45d
DylanWarrener <dqyqlqaqn@gmail.com>
2021-02-06 01:38:22 +0000

    Added new folders for each task in intro

--- Intro/task_1/task_1.py
+++ Intro/task_1/task_1.py
@@ -1,13 +1,3 @@
-'''
-Write a function that returns the sum of two numbers.
-
-For example, For param1 = 1 and param2 = 2, the output should be
-add(param1, param2) = 3.
-
-[input] - integer
-[output] - integer
-'''
-
 def add(param1, param2):
     return param1 + param2
 
