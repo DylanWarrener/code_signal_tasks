5f3d8a870d128a43c9fa75d2c56fca8c9bd10cfe
DylanWarrener <dqyqlqaqn@gmail.com>
2021-02-06 01:42:02 +0000

    Added a readme to each task in intro

---
+++ Intro/task_2/task_2 (README).md
@@ -0,0 +1,12 @@
+Given a year, return the century it is in. The first century spans 
+from the year 1 up to and including the year 100, the second - 
+from the year 101 up to and including the year 200, etc.
+
+For example, for year = 1905, the output should be 
+centuryFromYear(year) = 20;
+
+For year = 1700, the output should be
+centuryFromYear(year) = 17.
+
+[input] - integer year.
+[output] - integer.
--- Intro/task_3/task_3.py
+++ Intro/task_3/task_3.py
@@ -1,19 +1,3 @@
-'''
-Given the string, check if it is a palindrome.
-
-For example, for inputString = "aabaa", the output should be
-checkPalindrome(inputString) = true;
-
-For inputString = "abac", the output should be
-checkPalindrome(inputString) = false;
-
-For inputString = "a", the output should be
-checkPalindrome(inputString) = true.
-
-[input] - string
-[output] - boolean
-'''
-
 def checkPalindrome(inputString):
     if inputString[::-1] == inputString:
         return True
